091a93db410686985389842825fc07c4581af98a
msung6 <69650026+msung6@users.noreply.github.com>
2020-08-13 20:56:08 -0400

    web222

---
+++ src/script.js
@@ -0,0 +1,47 @@
+/** Add any JavaScript you need to this file. */
+
+
+
+
+
+
+
+function question(){
+
+let placeHolder=document.getElementById("question");
+
+
+let makeInput=document.createElement("input");
+makeInput.setAttribute("type","text");
+
+
+
+
+let textNode=document.createTextNode("hello");
+
+placeHolder.appendChild(makeInput);
+
+
+
+placeHolder.appendChild(textNode);
+}
+
+function comment(){
+let getType=document.getElementById("display");
+
+let makeInput=document.createElement("input");
+makeInput.setAttribute("type","text");
+
+
+
+
+}
+
+function order(){
+let getType=document.getElementById("display");
+
+let makeInput=document.createElement("input");
+makeInput.setAttribute("type","text");
+
+
+}
